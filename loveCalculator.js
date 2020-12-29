5037b1d97e1e5de93c37005aeed3a56a6e061588
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2020-12-28 19:10:58 -0500

    Add IF ELSE exercise

---
+++ loveCalculator.js
@@ -0,0 +1,16 @@
+prompt("What is your name?")
+prompt("What is their name?")
+
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "%. " + "You love each other like Narcissus loves Narcissus.")
+}
+if (loveScore >= 30 && loveScore <= 70) {
+    alert("Your love score is " + loveScore + "%.");
+}
+if (loveScore < 30) {
+    alert("Your love score is " + loveScore + "%. " + "You go together like cat and dog.")
+}
