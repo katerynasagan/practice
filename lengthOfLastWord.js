a19af9bd31cee5e7a7eeeabe3e5ed06a8565050d
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2021-01-06 00:54:10 -0500

    Add updated code

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -1,13 +1,11 @@
 var lengthOfLastWord = function(s) {
-  var splitted = s.split(" ");
-  var lastIndex = splitted.length - 1;
-  var lastWord = splitted[lastIndex];
+    var splitted = s.trim().split(" ");
+    var lastIndex = splitted.length - 1;
+    var lastWord = splitted[lastIndex];
 
-  if (s === " ") {
-    return 0;
-  } else if (lastWord === "") {
-    return splitted[lastIndex - 1].length;
-  } else {
-    return lastWord.length;
-  }
+   if (s === " ") {
+       return 0;
+   } else {
+       return lastWord.length;
+   }
 }
