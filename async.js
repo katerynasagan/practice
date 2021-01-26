527ee02abcfef86cc55d762c80aafc5e0bc3604a
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2021-01-25 23:11:12 -0500

    Add callback function exercise

---
+++ async.js
@@ -0,0 +1,27 @@
+console.log("Start");
+
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("Now we have the data")
+    callback({
+      userEmail: email
+    });
+  }, 3000);
+}
+
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2", "video3"]);
+  }, 2000);
+}
+
+const user = loginUser("mail@mail", 12345, user => {
+  console.log(user);
+  getUserVideos(user.userEmail, videos => {
+    console.log(videos);
+
+  });
+});
+
+
+console.log("End");
