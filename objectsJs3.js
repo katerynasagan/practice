35c31c2251433a24022e9808d87040b2c306b963
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2021-02-14 13:56:09 -0500

    Create objectsJs3.js

---
+++ objectsJs3.js
@@ -0,0 +1,77 @@
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
+
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Dave Brubeck",
+    "title": "Time Out",
+    "release_year": 1959,
+    "formats": [
+      "Vinyl",
+      "1 LP",
+      "180 gramm"
+    ],
+    "gold": true
+  }
+  // Add a record here
+];
+
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+  return "Not Found";
+  }
+  // Only change code above this line
+}
