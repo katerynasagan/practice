a094fc66c60af0fb16d3a79468d3e73e36d843e2
katerynasagan <73867153+katerynasagan@users.noreply.github.com>
2021-02-28 15:03:58 -0500

    Create conditionalOperator.js

---
+++ conditionalOperator.js
@@ -0,0 +1,14 @@
+function checkEqual(a, b) {
+ return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+
+function checkSign(num) {
+    return (num > 0) ? "positive"
+        : (num < 0) ? "negative"
+        : "zero";
+}
+
+checkSign(10);
